116e110bb7b47fcac037dbcb9b78a88b60078a6c
IGI-111 <igi-111@protonmail.com>
2021-03-20 04:26:24 +0100

    Update lualine config

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -1,29 +1,29 @@
-local lualine = require "lualine"
-lualine.options = {
-  theme = "palenight",
-  section_separators = {nil, nil},
-  component_separators = {nil, nil},
-  icons_enabled = false
-}
-lualine.sections = {
-  lualine_a = {"mode"},
-  lualine_b = {"branch"},
-  lualine_c = {"filename"},
-  lualine_x = {"encoding", "fileformat", "filetype"},
-  lualine_y = {
-    {"diagnostics", sources = {"nvim_lsp", "coc", "ale"}},
-    "progress"
+require("lualine").setup {
+  options = {
+    theme = "palenight",
+    section_separators = {nil, nil},
+    component_separators = {nil, nil},
+    icons_enabled = false
   },
-  lualine_z = {"location"}
-}
-lualine.inactive_sections = {
-  lualine_a = {},
-  lualine_b = {
-    {"diagnostics", sources = {"nvim_lsp", "coc", "ale"}}
+  sections = {
+    lualine_a = {"mode"},
+    lualine_b = {"branch"},
+    lualine_c = {"filename"},
+    lualine_x = {"encoding", "fileformat", "filetype"},
+    lualine_y = {
+      {"diagnostics", sources = {"nvim_lsp", "coc", "ale"}},
+      "progress"
+    },
+    lualine_z = {"location"}
   },
-  lualine_c = {"filename"},
-  lualine_x = {"location"},
-  lualine_y = {},
-  lualine_z = {}
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {
+      {"diagnostics", sources = {"nvim_lsp", "coc", "ale"}}
+    },
+    lualine_c = {"filename"},
+    lualine_x = {"location"},
+    lualine_y = {},
+    lualine_z = {}
+  }
 }
-lualine.status()
